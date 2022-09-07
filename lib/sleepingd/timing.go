d8e0f932d3000707d757112711b4bece70d95e06
Radon Rosborough <radon@intuitiveexplanations.com>
2022-09-06 19:18:50 -0700

    Rename

--- lib/sleepingd/timing.go
+++ lib/sleepingd/timing.go
@@ -10,14 +10,14 @@ type DeadMansSwitch struct {
 	ExpireCh <-chan struct{}
 }
 
-// GetDeadMansSwitch returns a DeadMansSwitch struct. This struct
+// NewDeadMansSwitch returns a DeadMansSwitch struct. This struct
 // contains two channels. The ExpireCh will receive an event
 // automatically after the provided timeout. However, at any time you
 // can send an event to the DelayCh to re-set this timeout to be from
-// the current time rather than from when the GetDeadMansSwitch was
+// the current time rather than from when the NewDeadMansSwitch was
 // returned. In other words, this lets you get notified automatically
 // some time after a process stops sending events.
-func GetDeadMansSwitch(timeout time.Duration) *DeadMansSwitch {
+func NewDeadMansSwitch(timeout time.Duration) *DeadMansSwitch {
 	delayCh := make(chan struct{})
 	expireCh := make(chan struct{})
 	timer := time.NewTimer(timeout)
