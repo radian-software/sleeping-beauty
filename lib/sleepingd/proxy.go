a85d2c80c10f8683a019749d97d0e8a9cb4495b7
Radon Rosborough <radon@intuitiveexplanations.com>
2022-09-07 21:24:12 -0700

    More test

--- lib/sleepingd/proxy.go
+++ lib/sleepingd/proxy.go
@@ -6,17 +6,35 @@ import (
 	"net"
 )
 
+// ProxyOptions is used to configure NewProxy, which see for
+// documentation. All fields are required except NewConnectionCh.
 type ProxyOptions struct {
-	Protocol        string
-	ListenAddr      string
-	UpstreamAddr    string
+	// Protocol is either "tcp" or "udp"
+	Protocol string
+	// ListenAddr is the address the proxy server will listen for
+	// incoming TCP/UDP traffic on, e.g. "127.0.0.1:80"
+	ListenAddr string
+	// UpstreamAddr is the upstream address the proxy will proxy
+	// TCP/UDP traffic to, e.g. "127.0.0.1:8080"
+	UpstreamAddr string
+	// NewConnectionCh is an optional channel. If it is provided
+	// then the channel will be sent an event every time there is
+	// a new incoming TCP/UDP connection, it is the caller's
+	// responsibility to make sure that events are read from this
+	// channel in a timely manner
 	NewConnectionCh *chan<- struct{}
 }
 
+// Proxy is a struct returned by NewProxy, that represents a running
+// proxy server. It can be used to stop the server by calling Close.
 type Proxy struct {
 	listener net.Listener
 }
 
+// NewProxy creates and starts a TCP or UDP server that will
+// transparently proxy TCP/UDP traffic to an upstream address, see
+// ProxyOptions for the options. An instance of Proxy is returned
+// which can be used to stop the server later.
 func NewProxy(opts *ProxyOptions) (*Proxy, error) {
 	l, err := net.Listen(opts.Protocol, opts.ListenAddr)
 	if err != nil {
--- lib/sleepingd/proxy_test.go
+++ lib/sleepingd/proxy_test.go
@@ -1,6 +1,7 @@
 package sleepingd
 
 import (
+	"fmt"
 	"io"
 	"io/ioutil"
 	"net"
@@ -55,4 +56,60 @@ func Test_Proxy_NoUpstream(t *testing.T) {
 	data, err := io.ReadAll(conn)
 	assert.NoError(t, err)
 	assert.Equal(t, "failed to dial upstream 127.0.0.1:7000: dial tcp 127.0.0.1:7000: connect: connection refused\n", string(data))
+}
+
+func getEchoserver(t *testing.T, protocol string, addr string) net.Listener {
+	l, err := net.Listen(protocol, addr)
+	assert.NoError(t, err)
+	go func() {
+		for {
+			conn, err := l.Accept()
+			if err != nil {
+				continue
+			}
+			go io.Copy(conn, conn)
+		}
+	}()
+	return l
+}
+
+func Test_Proxy_NewConnectionChannel(t *testing.T) {
+	echoserver := getEchoserver(t, "tcp", "127.0.0.1:7000")
+	defer echoserver.Close()
+	ch := make(chan struct{})
+	subch := chan<- struct{}(ch)
+	proxy, err := NewProxy(&ProxyOptions{
+		Protocol:        "tcp",
+		ListenAddr:      "127.0.0.1:7001",
+		UpstreamAddr:    "127.0.0.1:7000",
+		NewConnectionCh: &subch,
+	})
+	assert.NoError(t, err)
+	defer proxy.Close()
+	for i := 0; i < 5; i++ {
+		i := i
+		go func() {
+			message := fmt.Sprintf("message %d", i)
+			conn, err := net.Dial("tcp", "127.0.0.1:7001")
+			assert.NoError(t, err)
+			conn.Write([]byte(message))
+			data, err := io.ReadAll(conn)
+			assert.NoError(t, err)
+			assert.Equal(t, message, string(data))
+		}()
+	}
+	for i := 0; i < 5; i++ {
+		select {
+		case <-ch:
+			continue
+		case <-time.NewTimer(500 * time.Millisecond).C:
+			assert.Fail(t, "wasn't notified about connection #%d", i)
+		}
+	}
+	select {
+	case <-ch:
+		assert.Fail(t, "got notified about same connection multiple times")
+	case <-time.NewTimer(500 * time.Millisecond).C:
+		// expected
+	}
 }
