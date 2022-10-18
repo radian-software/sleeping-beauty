7693fc5a6cc2f9850fbd1e1e6f72f457663bc3bb
Radon Rosborough <radon@intuitiveexplanations.com>
2022-10-17 21:27:51 -0700

    Get integration test passing

--- test/integration/cases/basics_test.go
+++ test/integration/cases/basics_test.go
@@ -33,13 +33,15 @@ func Test_Basics(t *testing.T) {
 	sb := exec.Command("sleepingd")
 	sb.Env = append(
 		os.Environ(),
-		"SLEEPING_BEAUTY_COMMAND=python3 -m http.server -b 127.0.0.1 -d / 6666",
+		"SLEEPING_BEAUTY_COMMAND=python3 -u -m http.server -b 127.0.0.1 -d / 6666",
 		"SLEEPING_BEAUTY_TIMEOUT_SECONDS=1",
 		"SLEEPING_BEAUTY_COMMAND_PORT=6666",
 		"SLEEPING_BEAUTY_LISTEN_PORT=4444",
 	)
 	sbStdout := bytes.Buffer{}
 	sb.Stdout = &sbStdout
+	sbStderr := bytes.Buffer{}
+	sb.Stderr = &sbStderr
 	assert.NoError(t, sb.Start())
 	defer killNicely(t, sb.Process)
 	time.Sleep(500 * time.Millisecond)
@@ -53,6 +55,13 @@ func Test_Basics(t *testing.T) {
 		assert.Contains(t, curlStdout.String(), "Directory listing")
 		time.Sleep(2 * time.Second)
 	}
-	numStarts := strings.Count(sbStdout.String(), "Serving HTTP")
-	assert.Equal(t, 3, numStarts)
+	numProcStarts := strings.Count(sbStdout.String(), "Serving HTTP")
+	assert.Equal(t, 3, numProcStarts)
+	numProcRequests := strings.Count(sbStderr.String(), "GET / HTTP/1.1")
+	assert.Equal(t, 3, numProcRequests)
+	numDaemonStarts := strings.Count(sbStderr.String(), "starting subprocess")
+	assert.Equal(t, 3, numDaemonStarts)
+	numDaemonStops := strings.Count(sbStderr.String(), "stopping subprocess")
+	assert.Equal(t, 3, numDaemonStops)
+	assert.Contains(t, sbStderr.String(), "listening on 0.0.0.0:4444, proxying to 127.0.0.1:6666 with /usr/bin/bash command line: python3 -u -m http.server -b 127.0.0.1 -d / 6666")
 }
