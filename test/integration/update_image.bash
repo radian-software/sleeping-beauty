25008de79fa90c0f04f33eb4129466aad1eba878
Radon Rosborough <radon@intuitiveexplanations.com>
2022-10-17 21:57:48 -0700

    Setup GitHub Actions

---
+++ test/integration/update_image.bash
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+: "${GITHUB_TOKEN}"
+
+cd "$(dirname "$0")"
+
+ts="$(date +%s)"
+
+if [[ "$#" -eq 0 || "$1" != "-n" ]]; then
+    docker build . -t sleeping-beauty-integration-test:latest
+    ./run_in_docker.bash ./test/integration/run.bash
+fi
+
+image="ghcr.io/radian-software/sleeping-beauty-integration-test-ci:${ts}"
+
+echo "${GITHUB_TOKEN}" | docker login ghcr.io -u radian-software --password-stdin
+
+docker tag sleeping-beauty-integration-test:latest "${image}"
+docker push "${image}"
+
+echo "${image}" >./ci_image
