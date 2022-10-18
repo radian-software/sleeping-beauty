1b694d449d31fe689310b2e8fafab59f5f0902ed
Radon Rosborough <radon@intuitiveexplanations.com>
2022-10-17 18:43:48 -0700

    Fix race condition

--- lib/sleepingd/timing.go
+++ lib/sleepingd/timing.go
@@ -10,22 +10,26 @@ type DeadMansSwitch struct {
 	ExpireCh <-chan struct{}
 }
 
-// NewDeadMansSwitch returns a DeadMansSwitch struct. This struct
-// contains two channels. The ExpireCh will receive an event
-// automatically after the provided timeout. However, at any time you
-// can invoke the Delay method to re-set this timeout to be from the
-// current time rather than from when the NewDeadMansSwitch was
-// returned. In other words, this lets you get notified automatically
-// some time after a process stops sending events.
+// NewDeadMansSwitch returns a DeadMansSwitch struct. After getting
+// the struct back, invoke Delay to start the timer. Then ExpireCh
+// will receive an event automatically after the provided timeout.
+// However, at any time you can invoke the Delay method again to
+// re-set this timeout to be from the current time rather than from
+// when the NewDeadMansSwitch was returned. In other words, this lets
+// you get notified automatically some time after a process stops
+// sending events. If you invoke Delay again after the ExpireCh
+// receives an event, another event will be scheduled for the future.
 func NewDeadMansSwitch(timeout time.Duration) *DeadMansSwitch {
 	delayCh := make(chan struct{})
 	expireCh := make(chan struct{})
-	timer := time.NewTimer(timeout)
 	dms := DeadMansSwitch{
 		delayCh:  delayCh,
 		ExpireCh: expireCh,
 	}
+	var timer *time.Timer
 	go func() {
+		<-delayCh
+		timer = time.NewTimer(timeout)
 		for {
 			select {
 			case <-delayCh:
